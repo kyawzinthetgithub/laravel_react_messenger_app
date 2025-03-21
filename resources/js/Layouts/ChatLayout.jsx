cb62c3a5274c1d07373157b1cc110aca28472bac
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-21 23:46:20 +0630

    user login and home page

---
+++ resources/js/Layouts/ChatLayout.jsx
@@ -0,0 +1,8 @@
+import { usePage } from "@inertiajs/react";
+const ChatLayout = ({ children }) => {
+    const page = usePage();
+    console.log(page);
+    return <></>;
+};
+
+export default ChatLayout;
