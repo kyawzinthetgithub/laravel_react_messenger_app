57888902fa27256a1d412f59d6e6f7553befbe8d
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-27 00:05:13 +0630

    chat layout

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,6 +1,8 @@
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { usePage } from "@inertiajs/react";
 import { useEffect, useState } from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import TextInput from "@/Components/TextInput";
+import ConversationItem from "@/Components/App/ConversationItem";
 
 const ChatLayout = ({ children }) => {
     const page = usePage();
@@ -9,50 +11,64 @@ const ChatLayout = ({ children }) => {
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
 
-    const [onlineUsers,setOnlineUsers] = useState({}); //set online users
+    const [onlineUsers, setOnlineUsers] = useState({}); //set online users
 
     const isUserOnline = (userId) => onlineUsers[userId];
+
+    const onSearch = (e) => {
+        const search = e.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return (
+                    conversation.name.toLowerCase().includes(search)
+                );
+            })
+        );
+    };
+
     const [localConversations, setLocalConversations] = useState([]);
     const [sortedConversations, setSortedConversations] = useState([]);
 
     useEffect(() => {
         setSortedConversations(
             localConversations.sort((a, b) => {
-                if(a.blocked_at && b.blocked_at){
+                if (a.blocked_at && b.blocked_at) {
                     return a.blocked_at > b.blocked_at ? 1 : -1;
-                }else if(a.blocked_at){
+                } else if (a.blocked_at) {
                     return 1;
-                }
-                else if(b.blocked_at){
+                } else if (b.blocked_at) {
                     return -1;
                 }
 
-                if(a.last_message_date && b.last_message_date){
-                    return b.last_message_date.localCompare(a.last_message_date);
-                }
-                else if(a.last_message_date){
+                if (a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.localeCompare(
+                        a.last_message_date
+                    );
+                } else if (a.last_message_date) {
                     return -1;
-                }
-                else if(b.last_message_date){
+                } else if (b.last_message_date) {
                     return 1;
-                }
-                else{
+                } else {
                     return 0;
                 }
             })
         );
-    },[]);
+    }, []);
 
     useEffect(() => {
         setLocalConversations(conversations);
-    }
-    ,[conversations]); //set local conversations
+    }, [conversations]); //set local conversations
 
     useEffect(() => {
         Echo.join("online")
             .here((users) => {
-                const onlineUserObj = Object.fromEntries(users.map((user) => [user.id, user]));
-                setOnlineUsers((prevOnlineUsers) => ({ ...prevOnlineUsers, ...onlineUserObj }));
+                const onlineUserObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+                setOnlineUsers((prevOnlineUsers) => ({
+                    ...prevOnlineUsers,
+                    ...onlineUserObj,
+                }));
             })
             .joining((user) => {
                 setOnlineUsers((prevOnlineUsers) => {
@@ -71,12 +87,61 @@ const ChatLayout = ({ children }) => {
             .error((error) => {
                 console.error(error);
             });
-    },[]);
+    }, []);
 
     return (
         <>
-            ChatLayout Component
-            <div>{children}</div>
+            <div className="flex-1 w-full flex overflow-hidden">
+                {/* for sidebar */}
+                <div
+                    className={`transition-all w-full sm:w-[220px] md:w-[330px] bg-slate-800 flex flex-col overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
+                >
+                    <div className="flex justify-between items-center py-2 px-3 text-xl font-medium">
+                        {/* header */}
+                        My Conversations
+                        <div
+                            className="tooltip tooltip-left"
+                            data-tip="Create New Group"
+                        >
+                            <button
+                                type="button"
+                                className="text-gray-400 hover:text-gray-200"
+                            >
+                                <PencilSquareIcon className="w-4 h-4 ml-2 inline-block" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="p-3">
+                        <TextInput
+                            onKeyUp={onSearch}
+                            className="w-full"
+                            placeholder="Filter users and groups"
+                        />
+                    </div>
+                    <div className="flex-1 overflow-auto">
+                        {sortedConversations &&
+                            sortedConversations.map((conversation) => (
+                                <ConversationItem
+                                    key={`${
+                                        conversation.is_group
+                                            ? "group_"
+                                            : "user_"
+                                    }${conversation.id}`}
+                                    conversation={conversation}
+                                    online={!!isUserOnline(conversation.id)}
+                                    selectedConversation={selectedConversation}
+                                />
+
+                            ))}
+                    </div>
+                </div>
+                {/* for chat */}
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
+                </div>
+            </div>
         </>
     );
 };
